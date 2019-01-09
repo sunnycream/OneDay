a6c3f918013461402a8f1b67c516c68c5ada3bcd
liulinjie <liulinjie@marmotad.cn>
2019-01-09 11:16:34 +0800

    hud背景色

--- OneDay/OneDay/All/Extension/MBProgressHUD+SCProgressHUD.m
+++ OneDay/OneDay/All/Extension/MBProgressHUD+SCProgressHUD.m
@@ -11,19 +11,20 @@
 @implementation MBProgressHUD (SCProgressHUD)
 
 + (void)showTextOnly:(NSString *)text  {
-    [self showTextOnly:text toView:nil];
-}
-
-+ (void)showTextOnly:(NSString *)text toView:(UIView *)view {
-    if (view == nil) {
-        view = [[UIApplication sharedApplication].windows lastObject];
-    }
+    UIView *view = [[UIApplication sharedApplication].windows lastObject];
+//    UIView *view2 = [[[UIApplication sharedApplication] windows] lastObject];
+//    UIView *view3 = [[UIApplication sharedApplication].delegate window];
+//    UIView *view4 = [[UIApplication sharedApplication] keyWindow];
 
     MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
     hud.label.text = text;
+    hud.contentColor = [UIColor whiteColor];//文本颜色
     hud.mode = MBProgressHUDModeText;
     hud.backgroundView.style = MBProgressHUDBackgroundStyleSolidColor;
     hud.backgroundView.color = [UIColor colorWithWhite:0.0f alpha:0.2f];
+    //背景色
+    hud.bezelView.style = MBProgressHUDBackgroundStyleBlur;
+    hud.bezelView.color = kBarSelectedColor;
     [hud hideAnimated:YES afterDelay:3.0f];
 }
 
